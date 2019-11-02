20ba02461d80ee602c57728b1a2c0c5183c92075
thegleb <thegleb@users.noreply.github.com>
2019-11-01 23:30:25 -0700

    Initial model integration for sim

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,57 @@
+import numpy as np
+import tensorflow as tf
+
 from styx_msgs.msg import TrafficLight
+
+nn_graph_prefix = './light_classification/'
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
+        # self.graph = self.load_graph(nn_graph_prefix + 'ssd_inception_v2_1000steps/frozen_inference_graph.pb')
+        self.graph = self.load_graph(nn_graph_prefix + 'ssd_inception_v2_10000steps/frozen_inference_graph.pb')
+        self.image_tensor = self.graph.get_tensor_by_name('image_tensor:0')
+        self.detection_boxes = self.graph.get_tensor_by_name('detection_boxes:0')
+        self.detection_scores = self.graph.get_tensor_by_name('detection_scores:0')
+        self.detection_classes = self.graph.get_tensor_by_name('detection_classes:0')
+
+    def load_graph(self, graph_file):
+        """Loads a frozen inference graph"""
+        graph = tf.Graph()
+        with graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(graph_file, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+        return graph
+
+    def filter_boxes(self, min_score, boxes, scores, classes):
+        """Return boxes with a confidence >= `min_score`"""
+        n = len(classes)
+        idxs = []
+        for i in range(n):
+            if scores[i] >= min_score:
+                idxs.append(i)
+
+        filtered_boxes = boxes[idxs, ...]
+        filtered_scores = scores[idxs, ...]
+        filtered_classes = classes[idxs, ...]
+        return filtered_boxes, filtered_scores, filtered_classes
+
+    def to_image_coords(self, boxes, height, width):
+        """
+        The original box coordinate output is normalized, i.e [0, 1].
+
+        This converts it back to the original coordinate based on the image
+        size.
+        """
+        box_coords = np.zeros_like(boxes)
+        box_coords[:, 0] = boxes[:, 0] * height
+        box_coords[:, 1] = boxes[:, 1] * width
+        box_coords[:, 2] = boxes[:, 2] * height
+        box_coords[:, 3] = boxes[:, 3] * width
+
+        return box_coords
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -16,4 +64,45 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+
+
+        # image = preprocess(image)
+        image_np = np.expand_dims(np.asarray(image, dtype=np.uint8), 0)
+        with tf.Session(graph=self.graph) as sess:
+            # Actual detection.
+            (boxes, scores, classes) = sess.run(
+                [
+                    self.detection_boxes,
+                    self.detection_scores,
+                    self.detection_classes
+                ],
+                feed_dict={self.image_tensor: image_np}
+            )
+
+            # Remove unnecessary dimensions
+            boxes = np.squeeze(boxes)
+            scores = np.squeeze(scores)
+            classes = np.squeeze(classes)
+
+            confidence_cutoff = 0.4
+            # Filter boxes with a confidence score less than `confidence_cutoff`
+            boxes, scores, classes = self.filter_boxes(confidence_cutoff, boxes, scores, classes)
+            print(classes)
+            # The current box coordinates are normalized to a range between 0 and 1.
+            # This converts the coordinates actual location on the image.
+            # width, height = image.size
+            # adjusted_boxes = self.to_image_coords(boxes, height, width)
+            if len(scores) == 0:
+                return TrafficLight.UNKNOWN
+
+            value = max(scores)
+            idx = np.where(scores == value)
+            print('max [' + str(value) + '] for class [' + str(classes[value]) + ']')
+            if int(classes[idx]) == 1:
+                return TrafficLight.GREEN
+            elif int(classes[idx]) == 2:
+                return TrafficLight.RED
+            elif int(classes[idx]) == 3:
+                return TrafficLight.YELLOW
+            else:
+                return TrafficLight.UNKNOWN
--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -23,24 +23,11 @@ class TLDetector(object):
         self.camera_image = None
         self.lights = []
 
-        sub1 = rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
-        sub2 = rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
-
-        '''
-        /vehicle/traffic_lights provides you with the location of the traffic light in 3D map space and
-        helps you acquire an accurate ground truth data source for the traffic light
-        classifier by sending the current color state of all traffic lights in the
-        simulator. When testing on the vehicle, the color state will not be available. You'll need to
-        rely on the position of the light and the camera image to predict it.
-        '''
-        sub3 = rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray, self.traffic_cb)
-        sub6 = rospy.Subscriber('/image_color', Image, self.image_cb)
-
         config_string = rospy.get_param("/traffic_light_config")
         self.config = yaml.load(config_string)
 
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
-        self.image_saver_pub = rospy.Publisher('/image_to_save', Image, queue_size=1)
+        # self.image_saver_pub = rospy.Publisher('/image_to_save', Image, queue_size=1)
 
         self.bridge = CvBridge()
         self.light_classifier = TLClassifier()
@@ -55,6 +42,19 @@ class TLDetector(object):
 
         self.waypoints = None
         self.waypoints_tree = None
+
+        sub1 = rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
+        sub2 = rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
+
+        '''
+        /vehicle/traffic_lights provides you with the location of the traffic light in 3D map space and
+        helps you acquire an accurate ground truth data source for the traffic light
+        classifier by sending the current color state of all traffic lights in the
+        simulator. When testing on the vehicle, the color state will not be available. You'll need to
+        rely on the position of the light and the camera image to predict it.
+        '''
+        sub3 = rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray, self.traffic_cb)
+        sub6 = rospy.Subscriber('/image_color', Image, self.image_cb)
 
         rospy.spin()
 
@@ -101,11 +101,11 @@ class TLDetector(object):
             self.upcoming_red_light_pub.publish(Int32(self.last_wp))
         self.state_count += 1
 
-        self.image_save_counter += 1
-        # save frames only every 5 frames or so
-        if self.image_save_counter == 9:
-            self.image_saver_pub.publish(msg)
-            self.image_save_counter = 0
+        # self.image_save_counter += 1
+        # # save frames only every 10 frames or so
+        # if self.image_save_counter == 9:
+        #     self.image_saver_pub.publish(msg)
+        #     self.image_save_counter = 0
 
     def get_closest_waypoint(self, x, y):
         """Identifies the closest path waypoint to the given position
@@ -129,15 +129,13 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        return light.state
-        # if(not self.has_image):
-        #     self.prev_light_loc = None
-        #     return False
-        #
-        # cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
-        #
-        # #Get classification
-        # return self.light_classifier.get_classification(cv_image)
+        if not self.has_image:
+            self.prev_light_loc = None
+            return False
+
+        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+
+        return self.light_classifier.get_classification(cv_image)
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
